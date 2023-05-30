e3dd7300f6bf45a565448a5b4c4e471d6143090f
saeed <saeed.all.gharaee@fkt42.de>
2023-05-30 16:37:48 +0200

    login added

---
+++ avicenna_app/lib/src/auth/presentation/pages/auth_page.dart
@@ -0,0 +1,52 @@
+import 'package:avicenna_app/src/auth/presentation/bloc/auth_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AuthPage extends StatefulWidget {
+  AuthPage({super.key});
+
+  final AuthBloc _bloc = AuthBloc();
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  final AuthBloc _bloc = AuthBloc();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AuthBloc, AuthState>(
+      bloc: _bloc,
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('LOGIN'),
+          ),
+          body: SafeArea(
+            child: SingleChildScrollView(
+                child: Column(
+              children: [
+                TextField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    label: Text('Email'),
+                  ),
+                ),
+                TextField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(label: Text('Email')),
+                ),
+                ElevatedButton(
+                    onPressed: () => _bloc.add(LoginEvent()),
+                    child: const Text("Login"))
+              ],
+            )),
+          ),
+        );
+      },
+    );
+  }
+}
