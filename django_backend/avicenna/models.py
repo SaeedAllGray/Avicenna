f9b7c4b1db2012c6cd242801a86f9e7ab79480d3
P1car00n <80679567+P1car00n@users.noreply.github.com>
2023-06-13 21:04:06 +0200

    Implement the Doctor, Patient and Appointment classes

--- django_backend/avicenna/models.py
+++ django_backend/avicenna/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=30)
+    specialization = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+    appointment_timestamp = models.DateTimeField('appointemnt\'s date')
