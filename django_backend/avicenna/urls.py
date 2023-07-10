e7ccf37c88cc788f317ca2e5f1e75d96eeb4ba89
P1car00n <80679567+P1car00n@users.noreply.github.com>
2023-07-10 22:32:23 +0200

    Implement basic views

--- django_backend/avicenna/urls.py
+++ django_backend/avicenna/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:user_id>/delete/", views.delete_user, name="delete_user"),
+    path("<int:user_id>/create/", views.create_user, name="create_user"),
+    path("<int:user_id>/update/", views.update_user, name="update_user"),
+    path("<int:user_id>/register/", views.register_user, name="register_user"),
+    path("<int:user_id>/login/", views.log_in_user, name="log_in_user"),
 ]
--- django_backend/avicenna/views.py
+++ django_backend/avicenna/views.py
@@ -3,3 +3,21 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the avicenna index.")
+
+def delete_user(request, user_id):
+    return HttpResponse("You've deleted user #%s." % user_id)
+
+
+def create_user(request, user_id):
+    response = "You've created user #%s."
+    return HttpResponse(response % user_id)
+
+
+def update_user(request, user_id):
+    return HttpResponse("You've updated user #%s." % user_id)
+
+def register_user(request, user_id):
+    return HttpResponse("You've registered user #%s." % user_id)
+
+def log_in_user(request, user_id):
+    return HttpResponse("You've logged in as user #%s." % user_id)
