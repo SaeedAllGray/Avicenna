a6b91469c2e49bfea2dbf1328a0da5a9d612debf
P1car00n <80679567+P1car00n@users.noreply.github.com>
2023-07-11 11:12:02 +0200

    Implement getting appointments by user_id

--- django_backend/avicenna/views.py
+++ django_backend/avicenna/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
+from .models import Appointment
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the avicenna index.")
+
 
 def delete_user(request, user_id):
     return HttpResponse("You've deleted user #%s." % user_id)
@@ -16,8 +20,17 @@ def create_user(request, user_id):
 def update_user(request, user_id):
     return HttpResponse("You've updated user #%s." % user_id)
 
+
 def register_user(request, user_id):
     return HttpResponse("You've registered user #%s." % user_id)
 
+
 def log_in_user(request, user_id):
     return HttpResponse("You've logged in as user #%s." % user_id)
+
+
+def get_appointments_by_user_id(request, user_id):
+    appointment = get_list_or_404(
+        Appointment.objects.order_by('appointment_timestamp'),
+        doctor__user__id__exact=user_id)  # TODO: make it support both patients and doctors
+    return HttpResponse(appointment)
