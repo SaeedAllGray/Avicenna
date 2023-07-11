deeaa02117eca025f90f916a42d8c647bc648b4e
P1car00n <80679567+P1car00n@users.noreply.github.com>
2023-07-11 17:44:56 +0200

    Create serializers

--- django_backend/avicenna/serializers.py
+++ django_backend/avicenna/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Appointment, CustomUser
+from .models import Appointment, CustomUser, Patient, Doctor
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
@@ -12,4 +12,16 @@ class AppointmentSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['doctor', 'patient', 'appointment_timestamp']
+        fields = ['email', 'username', 'first_name', 'last_name', 'is_doctor', 'is_patient']
+
+class DoctorSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Doctor
+        fields = ['user', 'phone_number', 'specialization', 'address']
+
+class PatientSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Patient
+        fields = ['user', 'ssn']
