80e2d7fb289ca59bb79680541d7cb70bec5788ef
P1car00n <80679567+P1car00n@users.noreply.github.com>
2023-07-12 17:44:46 +0200

    Fix erros from faulty merging

--- django_backend/avicenna/models.py
+++ django_backend/avicenna/models.py
@@ -50,14 +50,6 @@ class Patient(models.Model):
         return str(self.user)
 
 
-class Patient(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-
-    def __str__(self):
-        return self.name
-
-
 class Appointment(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
