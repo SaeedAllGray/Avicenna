14b7be8fcdb4a39037b3961b456a38e1d09632c5
saeed <saeed.all.gharaee@fkt42.de>
2023-07-13 15:54:41 +0200

    bugfix

--- avicenna_app/lib/src/splash/bloc/splash_bloc.dart
+++ avicenna_app/lib/src/splash/bloc/splash_bloc.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 part 'splash_event.dart';
