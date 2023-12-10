d308ee4b6ccb22dc213c5a0c209625a2414327d9
navidallgharaee <NAllGharaee@netTrek.de>
2023-12-10 10:31:58 +0100

    Added some presentation components.

---
+++ avicenna_app/lib/presentation/constants/colors.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color BLACK = Colors.black;
+  static const Color primary = Colors.teal;
+}
---
+++ avicenna_app/lib/presentation/constants/fonts.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:avicenna_app/presentation/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class FontStyles {
+  static const BLACK_MEDIUM_24 = TextStyle(
+    color: AppColors.BLACK,
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+  );
+  static const BLACK_REGULAR_18 = TextStyle(fontSize: 18.0);
+}
