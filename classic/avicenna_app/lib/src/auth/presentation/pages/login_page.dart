5c1ad35851384a33a30083a423468cc5286fe468
SaeedAllGray <saeedag.imail@gmail.com>
2023-12-25 16:03:10 +0100

    icons added

--- classic/avicenna_app/lib/src/auth/presentation/pages/login_page.dart
+++ classic/avicenna_app/lib/src/auth/presentation/pages/login_page.dart
@@ -21,74 +21,74 @@ class _LoginPageState extends State<LoginPage> {
     return BlocProvider(
       create: (context) => _bloc,
       child: BlocListener<AuthBloc, AuthState>(
-          listener: (context, state) {
-            if (state is AuthSucceedState) {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => const HomePage()));
-            }
-          },
-          child: BlocBuilder<AuthBloc, AuthState>(
-            bloc: _bloc,
-            builder: (context, state) {
-              return Scaffold(
-                appBar: AppBar(
-                  title: const Text('LOGIN'),
-                ),
-                body: SafeArea(
-                  child: SingleChildScrollView(
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          controller: usernameController,
-                          decoration: const InputDecoration(
-                            label: Text('Username'),
-                          ),
-                          onChanged: (v) => _bloc.add(InputEvent()),
+        listener: (context, state) {
+          if (state is AuthSucceedState) {
+            Navigator.pushReplacement(context,
+                MaterialPageRoute(builder: (context) => const HomePage()));
+          }
+        },
+        child: BlocBuilder<AuthBloc, AuthState>(
+          bloc: _bloc,
+          builder: (context, state) {
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('LOGIN'),
+              ),
+              body: SafeArea(
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: usernameController,
+                        decoration: const InputDecoration(
+                          label: Text('Username'),
                         ),
-                        TextField(
-                          obscureText: true,
-                          controller: passwordController,
-                          decoration:
-                              const InputDecoration(label: Text('Password')),
-                          onChanged: (v) => _bloc.add(InputEvent()),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        state is AuthInProgress
-                            ? const Center(
-                                child: CircularProgressIndicator.adaptive(),
-                              )
-                            : Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  TextButton(
-                                      onPressed: () =>
-                                          Navigator.pushReplacement(
-                                              context,
-                                              MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      const SignupPage())),
-                                      child: const Text("I AM A NEW USER")),
-                                  ElevatedButton(
-                                      onPressed: usernameController
-                                                  .text.isNotEmpty &&
-                                              passwordController.text.isNotEmpty
-                                          ? () => _bloc.add(LoginEvent(
-                                              usernameController.text,
-                                              passwordController.text))
-                                          : null,
-                                      child: const Text("Login"))
-                                ],
-                              )
-                      ],
-                    ),
+                        onChanged: (v) => _bloc.add(InputEvent()),
+                      ),
+                      TextField(
+                        obscureText: true,
+                        controller: passwordController,
+                        decoration:
+                            const InputDecoration(label: Text('Password')),
+                        onChanged: (v) => _bloc.add(InputEvent()),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      state is AuthInProgress
+                          ? const Center(
+                              child: CircularProgressIndicator.adaptive(),
+                            )
+                          : Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                TextButton(
+                                    onPressed: () => Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const SignupPage())),
+                                    child: const Text("I AM A NEW USER")),
+                                ElevatedButton(
+                                    onPressed: usernameController
+                                                .text.isNotEmpty &&
+                                            passwordController.text.isNotEmpty
+                                        ? () => _bloc.add(LoginEvent(
+                                            usernameController.text,
+                                            passwordController.text))
+                                        : null,
+                                    child: const Text("Login"))
+                              ],
+                            )
+                    ],
                   ),
                 ),
-              );
-            },
-          )),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
