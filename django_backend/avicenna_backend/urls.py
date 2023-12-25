5e47bd65e1f02e80e3a73a4adfd718d896ffe12e
Arthur Zevaloff <80679567+P1car00n@users.noreply.github.com>
2023-12-25 20:51:00 +0100

    Update the project urls

--- django_backend/avicenna_backend/urls.py
+++ django_backend/avicenna_backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("avicenna.urls")),
+    path("api/", include("avicenna.urls")),
     path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
 ]
