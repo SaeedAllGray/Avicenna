6e3045cd673fd40952bfc01aa8edcd425ae4483c
Arthur Zevaloff <80679567+P1car00n@users.noreply.github.com>
2024-01-20 14:53:49 +0100

    Implement custom PageNumberPagination

--- django_backend/avicenna_backend/settings.py
+++ django_backend/avicenna_backend/settings.py
@@ -139,8 +139,6 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.UserRateThrottle",
     ],
     "DEFAULT_THROTTLE_RATES": {"user": "1000/day"},
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 20,
 }
 
 
